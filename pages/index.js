eff923303460b276fcf517be8a7517341f8ec912
nikolascatur <nikolas@ruangguru.com>
2022-11-08 14:17:25 +0700

    feat: implement dynamic route

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import { getSortedPostsData } from "../lib/post";
 import utilStyles from "../styles/utils.module.css";
-
+import Link from "next/link";
+import Date from "../components/date";
 // in development (yarn dev or npm run dev) run when request, but in production only run in buildtime
 // so this function is not recommended to get data dynamicaly, if you want fetch data from server you can use getServerSideProps
 export async function getStaticProps() {
@@ -23,11 +24,11 @@ export default function Home({ allPostsData }) {
           {allPostsData.map((post) => {
             return (
               <li className={utilStyles.listItme} key={post.id}>
-                {post.title}
+                <Link href={`/post/${post.id}`}>{post.title}</Link>
                 <br />
-                {post.id}
-                <br />
-                {post.date}
+                <small className={utilStyles.lightText}>
+                  <Date dateString={post.date} />
+                </small>
               </li>
             );
           })}
